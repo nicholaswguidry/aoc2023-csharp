e32599ab801d7cc77e9bd2e1d34a8f7f55409d9d
Nicholas W. Guidry <nick@avibeweb.com>
2023-12-03 13:05:40 -0800

    day 1 part 1 solution

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+//regex
+using System.Text.RegularExpressions;
 class Program
 {
     static void Main()
@@ -8,11 +10,30 @@ class Program
         using (StreamReader sr = File.OpenText(path))
         {
             string s = "";
-
+            int total = 0;
             while ((s = sr.ReadLine()) != null)
             {
-                // do something here for each line of the input, `s`
+                // start a digit List
+                List<char> digits = new List<char>();
+                // Regex to get the digits
+                digits = Regex.Matches(s, @"\d").Cast<Match>().Select(m => m.Value[0]).ToList();
+
+                if (digits.Count == 0)
+                {
+                    // if there are no digits in the line, skip it
+                    continue;
+                }
+                else if (digits.Count == 1)
+                {
+                    digits.Add(digits[0]);
+                }
+
+
+                // Console.WriteLine("{0}{1}", digits[0], digits[digits.Count - 1]);
+                int twoDigitVal = int.Parse(digits[0].ToString() + digits[digits.Count - 1]);
+                total += twoDigitVal;
             }
+            Console.WriteLine($"Total: {total}");
         }
     }
 }
