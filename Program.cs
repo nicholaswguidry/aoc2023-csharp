aeb54aafb71b5548d6ad17b130bcb53471375031
Nicholas W. Guidry <nick@avibeweb.com>
2023-12-03 12:58:31 -0800

    initial line read template commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        string path = "./input.txt";
+
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string s = "";
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                // do something here for each line of the input, `s`
+            }
+        }
+    }
+}
