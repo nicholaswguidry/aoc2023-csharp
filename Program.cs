d562d1c56fb65727c549b0d3d6db7d1ade7bcd05
Nicholas W. Guidry <nick@avibeweb.com>
2023-12-03 21:00:27 -0800

    day 2 part 1 solution

--- Program.cs
+++ Program.cs
@@ -8,19 +8,83 @@ class Program
         using (StreamReader sr = File.OpenText(path))
         {
             string s = "";
-
+            int total = 0;
             while ((s = sr.ReadLine()) != null)
             {
                 // do something here for each line of the input, `s`
 
                 //parse the integer in the digit-only substring until we hit the first `:`
                 //gameString is the string of the game number
+                int colonIndex = s.IndexOf(':');
                 string gameString = Regex.Match(s.Substring(0, s.IndexOf(':')), @"\d+").Value;
 
-                int firstInt = int.Parse(gameString);
-                Console.WriteLine($"This is Game: {firstInt}");
+                int gameInt = int.Parse(gameString);
+
+                // between colonIndex and the first ; is set 1
+                // create sets between ; and the end of the line
+
+                List<int> lineRed = new List<int>();
+                List<int> lineBlue = new List<int>();
+                List<int> lineGreen = new List<int>();
+                while (colonIndex < s.Length)
+                {
+                    int setStart = colonIndex;
+                    int setEnd = s.IndexOf(';', setStart + 1);
+                    if (setEnd == -1)
+                    {
+                        setEnd = s.Length;
+                    }
+                    string setString = s.Substring(setStart + 1, setEnd - setStart - 1);
+
+
+
+                    int redScore = ParseScore(setString, "red");
+                    lineRed.Add(redScore);
+
+                    int blueScore = ParseScore(setString, "blue");
+                    lineBlue.Add(blueScore);
+
+                    int greenScore = ParseScore(setString, "green");
+                    lineGreen.Add(greenScore);
+
+                    colonIndex = setEnd;
+                }
+
+                int[] redArray = lineRed.ToArray();
+                int[] blueArray = lineBlue.ToArray();
+                int[] greenArray = lineGreen.ToArray();
+
+                bool countGame = (
+                    blueArray.Max() <= 14 &&
+                    redArray.Max() <= 12 &&
+                    greenArray.Max() <= 13
+                );
+
+                if (countGame)
+                {
+                    total += gameInt;
+                }
 
             }
+            Console.WriteLine($"Total: {total}");
         }
+
+    }
+
+    static int ParseScore(string scoreString, string color)
+    {
+        // remove the ' red' and get the int after finding match
+        string num = Regex.Match(scoreString, @"\d+ " + color).Value;
+        if (num == "")
+        {
+            num = "0";
+        }
+        else
+        {
+            num = num.Replace(" " + color, "");
+        }
+        int score = int.Parse(num);
+
+        return score;
     }
 }
