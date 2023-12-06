b7c618879ad7dd43c375c39445f517e273523656
Nicholas W. Guidry <nick@avibeweb.com>
2023-12-05 21:31:11 -0800

    day 6 part 1 solution

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Text.RegularExpressions;
+
+
 class Program
 {
     static void Main()
@@ -8,11 +10,36 @@ class Program
         using (StreamReader sr = File.OpenText(path))
         {
             string s = "";
-
+            List<string> lines = new List<string>();
             while ((s = sr.ReadLine()) != null)
             {
-                // do something here for each line of the input, `s`
+                lines.Add(s);
             }
+
+            //get the digits of line 0
+            Match[] times = Regex.Matches(lines[0], @"\d+").Cast<Match>().ToArray();
+            //get the digits of line 1
+            Match[] distances = Regex.Matches(lines[1], @"\d+").Cast<Match>().ToArray();
+            int winProduct = 1;
+
+            for (int i = 0; i < times.Length; i++)
+            {
+                int winning = 0;
+                for (int j = 1; j < int.Parse(times[i].Value); j++)
+                {
+
+                    int movementTime = int.Parse(times[i].Value) - j;
+                    if (j * movementTime > int.Parse(distances[i].Value))
+                    {
+                        winning++;
+                    }
+                }
+
+                winProduct *= winning;
+
+            }
+            Console.WriteLine($"Product is {winProduct}");
         }
     }
 }
+
